d5ad39f605a08c92ffd2064c6f141999991032ff
Pingu-Io <52334818+Pingu-Io@users.noreply.github.com>
2021-06-26 02:05:56 +0200

    Added check for microphone enabled (Ts3 status)

--- client.lua
+++ client.lua
@@ -1,12 +1,13 @@
 isPluginActive = false
 isMicActive = false
+isMicEnabled = false
 
--- Event that check the status of the ts3 plugin (-1=notActive, 0,1,2,3=active)
+-- Event that check the status of the ts3 plugin
 AddEventHandler('SaltyChat_PluginStateChanged', function(pluginState)
-    if(pluginState == -1) then
-        isPluginActive = false
-    else
+    if(pluginState == 2 or pluginState == 3) then
         isPluginActive = true
+    else
+        isPluginActive = false
     end
 
     SendNUIMessage({
@@ -29,18 +30,32 @@ AddEventHandler('SaltyChat_MicStateChanged', function(isMicrophoneMuted)
     })
 end)
 
+-- Event that check if a player microphone is enabled or not
+AddEventHandler('SaltyChat_MicEnabledChanged', function(isMicrophoneEnabled)
+    if(isMicrophoneEnabled) then
+        isMicEnabled = true
+    else
+        isMicEnabled = false
+    end
+
+    SendNUIMessage({
+        action = "updateValues",
+        isMicEnabledStatus = isMicEnabled
+    })
+end)
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
 
-        if(isPluginActive and isMicActive) then
+        if(isPluginActive and isMicActive and isMicEnabled) then
             SendNUIMessage({
                 action = "setWindow",
                 value = false
             })
 
             EnableAllControlActions(0)
-        elseif(not isPluginActive or not isMicActive) then
+        elseif(not isPluginActive or not isMicActive or not isMicEnabled) then
             SendNUIMessage({
                 action = "setWindow",
                 value = true
